84e5c97f0641e55963825e95039a55f43b90b012
Sujan khadka <sujan2005k@gmail.com>
2025-06-01 16:13:51 +0545

    initial commit

---
+++ src/app/(marketing)/layout.tsx
@@ -0,0 +1,11 @@
+import { ReactNode } from "react";
+
+export default function MarketingLayout({children} : { children : ReactNode}) {
+
+
+    return <div className="selection:bg-[hsl(320,65%,52%,20%)] ">
+             
+        blah blah
+             {children}
+    </div>
+}
